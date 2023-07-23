1456e542e4112ea1ddcd418915055a7c36258ae8
elisemcclain <celisemcclain@gmail.com>
2023-07-23 11:12:51 -0500

    python functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(name = "Guido"):
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
-def halve(number):
-    pass
+def halve(number = "12"):
+    return number / 2
